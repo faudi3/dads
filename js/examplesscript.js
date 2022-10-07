a0c5c08271da28c8267059f44ad17fc7776662d7
faudi <alexnagornyi17@gmail.com>
2022-10-07 17:24:04 +0300

    adapting

--- js/examplesscript.js
+++ js/examplesscript.js
@@ -4,12 +4,23 @@ const sidebar = document.querySelector(".sidebar");
 const mainSlide = document.querySelector(".main-slide");
 const container = document.querySelector(".container");
 const slidesCount = mainSlide.querySelectorAll(".examples__block").length;
-
+const slides1 = document.querySelectorAll(".examples__block");
 let player = document.querySelector(".vid");
+let descrip = document.querySelectorAll(".sidebar div");
 
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+descrip[9].style.display = "block";
+setTimeout(() => {
+  console.log(slides1);
+}, 1000);
+
+if (container.clientWidth > 1000) {
+  sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+} else {
+  sidebar.style.top = 0;
+}
 
 let activeSlideIdx = 0;
+let activeDesc = 9;
 
 upBtn.addEventListener("click", () => {
   changeSlide("up");
@@ -28,40 +39,85 @@ downBtn.addEventListener("click", () => {
 });
 
 function changeSlide(direction) {
-  if (direction === "up") {
-    activeSlideIdx++;
-    if (activeSlideIdx === slidesCount) {
-      activeSlideIdx = 0;
+  if (container.clientWidth < 1000) {
+    if (direction === "up") {
+      if (activeSlideIdx !== slidesCount - 1) {
+        slides1[activeSlideIdx].classList.remove("activeHor");
+        slides1[activeSlideIdx + 1].classList.add("activeHor");
+
+        console.log(
+          activeSlideIdx,
+          activeDesc,
+          descrip[activeDesc],
+          descrip[activeDesc - 1]
+        );
+        descrip[activeDesc].style.display = "none";
+        descrip[activeDesc - 1].style.display = "block";
+        activeSlideIdx++;
+        activeDesc--;
+      } else {
+        descrip[activeDesc].style.display = "none";
+        slides1[activeSlideIdx].classList.remove("activeHor");
+
+        activeSlideIdx = 0;
+        activeDesc = 9;
+        slides1[activeSlideIdx].classList.add("activeHor");
+        descrip[activeDesc].style.display = "block";
+      }
+    } else {
+      if (activeSlideIdx !== slidesCount - 1) {
+        slides1[activeSlideIdx].classList.remove("activeHor");
+        slides1[activeSlideIdx + 1].classList.add("activeHor");
+
+        descrip[activeDesc].style.display = "none";
+        descrip[activeDesc - 1].style.display = "block";
+        activeSlideIdx++;
+        activeDesc--;
+      } else {
+        descrip[activeDesc].style.display = "none";
+        slides1[activeSlideIdx].classList.remove("activeHor");
+
+        activeSlideIdx = 0;
+        activeDesc = 9;
+        slides1[activeSlideIdx].classList.add("activeHor");
+        descrip[activeDesc].style.display = "block";
+      }
     }
-  } else if (direction === "down") {
-    activeSlideIdx--;
-    if (activeSlideIdx < 0) {
-      activeSlideIdx = slidesCount - 1;
+  } else {
+    if (direction === "up") {
+      activeSlideIdx++;
+      if (activeSlideIdx === slidesCount) {
+        activeSlideIdx = 0;
+      }
+    } else if (direction === "down") {
+      activeSlideIdx--;
+      if (activeSlideIdx < 0) {
+        activeSlideIdx = slidesCount - 1;
+      }
     }
+    const height = container.clientHeight;
+    mainSlide.style.transform = `translateY(-${activeSlideIdx * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIdx * height}px)`;
   }
-  const height = container.clientHeight;
-  mainSlide.style.transform = `translateY(-${activeSlideIdx * height}px)`;
-  sidebar.style.transform = `translateY(${activeSlideIdx * height}px)`;
 }
+//for stopping vid
 
-let SliderLists = document.querySelectorAll(".examples__block");
-
-SliderLists.forEach((item, ind) => {
-  let player = item.querySelector(".vid");
-
-  upBtn.addEventListener("click", () => {
-    changeSlide("up");
-    player.contentWindow.postMessage(
-      '{"event":"command","func":"pauseVideo","args":""}',
-      "*"
-    );
-  });
-
-  downBtn.addEventListener("click", () => {
-    changeSlide("down");
-    player.contentWindow.postMessage(
-      '{"event":"command","func":"pauseVideo","args":""}',
-      "*"
-    );
-  });
-});
+// let SliderLists = document.querySelectorAll(".examples__block");
+// SliderLists.forEach((item, ind) => {
+//   let player = item.querySelector(".vid");
+//   upBtn.addEventListener("click", () => {
+//     changeSlide("up");
+//     player.contentWindow.postMessage(
+//       '{"event":"command","func":"pauseVideo","args":""}',
+//       "*"
+//     );
+//   });
+//
+//   downBtn.addEventListener("click", () => {
+//     changeSlide("down");
+//     player.contentWindow.postMessage(
+//       '{"event":"command","func":"pauseVideo","args":""}',
+//       "*"
+//     );
+//   });
+// });
